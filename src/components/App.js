3f987a7939cd5f12a70e38518e7a55d1d5fe05ae
Maduri Vassaramo <maduri@vinterior.co>
2021-04-30 10:13:14 +0200

    add state to App class

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 class App extends React.Component {
+  state = { images: [] }
 
   async onSearchSubmit(term) {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
@@ -10,15 +11,15 @@ class App extends React.Component {
         Authorization: 'Client-ID QDxnDPpiMqDISnd0eAUhu3A6o7LY2x7tb1NkYOqAAGI'
       }
     })
-
-    console.log(response.data.results)
+    this.setState({ images: response.data.results })
   }
 
   render() {
 
     return(
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={(term) => this.onSearchSubmit(term)}/>
+        Found {this.state.images.length} Images
       </div>
     )
   }
